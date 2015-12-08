2ac8929d294afeb93be1319775653ffbc778eddf
giacomocavallocg <giacomocavallo@mbp-di-giacomo.lan>
2015-12-08 12:34:19 +0100

    GestioneAnnuncio: 	Ho aggiunto dei primi metodi per l'inserimento "smart", ci sono un po di commenti inutili, mi servono ora poi li cancello !!
    STAY HUNGRY STAY SSCSWEB

--- haiHome-ejb/src/java/ejb/GestoreAnnuncio.java
+++ haiHome-ejb/src/java/ejb/GestoreAnnuncio.java
@@ -6,8 +6,57 @@
 package ejb;
 
 import entity.Annuncio;
+import entity.Città;
 import entity.Locatore;
+import facade.AnnuncioFacadeLocal;
+import facade.CittàFacadeLocal;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateful;
+
+
+/*
+    INIZIALI
+    private Locatore locatore;
+
+    INDIRIZZO
+    private String indirizzo;
+    @ManyToOne
+    private Città città;
+    private String quartiere;
+    private double[] latLng;
+
+
+    APPARTAMENTO
+    private String descrizione;
+    private double metratura;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataInizioAffitto;
+    private int numeroStanze;
+
+
+    STANZE
+    @OneToMany
+    private Collection<Stanza> listaStanza;
+
+
+    COSTI
+    private boolean compresoCondominio;
+    private boolean compresoRiscaldamento;
+    private double prezzo = 0;
+
+
+    FINALE
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataPubblicazione;
+*/
+
+
+
+
+
+
 
 /**
  *
@@ -15,6 +64,11 @@ import javax.ejb.Stateful;
  */
 @Stateful
 public class GestoreAnnuncio implements GestoreAnnuncioLocal {
+    
+    @EJB
+    private CittàFacadeLocal cittàFacade;
+    @EJB
+    private AnnuncioFacadeLocal annuncioFacade;
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
@@ -39,8 +93,52 @@ public class GestoreAnnuncio implements GestoreAnnuncioLocal {
             return true;
         else
             return false;
-
     }
+
+    @Override
+    public boolean inserisciInfoIndirizzo(String cittaStr, String quartiere, String indirizzo, double[] latlng) {
+       if(this.annuncio==null)
+           return false;
+       
+       //recupero la città dal nome
+       Città città = new Città();
+       List<Città> mieCittà = cittàFacade.findAll();
+       boolean trovato=false;
+       int i=0;
+       while(!trovato){
+           Città c = mieCittà.get(i);
+           if(c.getNome().equalsIgnoreCase(cittaStr)){
+               città = c;
+               trovato = true;
+           }
+       }
+
+               
+       this.annuncio.setCittà(città);
+       this.annuncio.setQuartiere(quartiere);
+       this.annuncio.setIndirizzo(indirizzo);
+       this.annuncio.setLatLng(latlng);
+       return true;
+    }
+
+    @Override
+    public boolean inserisciInfoAppartamento(String descrizione, double metratura, Date dataInizioAffitto, int numeroStanze) {
+        this.annuncio.setDescrizione(descrizione);
+        this.annuncio.setMetratura(metratura);
+        this.annuncio.setDataInizioAffitto(dataInizioAffitto);
+        this.annuncio.setNumeroStanze(numeroStanze);
+        return true;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
--- haiHome-ejb/src/java/ejb/GestoreAnnuncioLocal.java
+++ haiHome-ejb/src/java/ejb/GestoreAnnuncioLocal.java
@@ -5,11 +5,20 @@
  */
 package ejb;
 
+import entity.Città;
 import entity.Locatore;
+import entity.Quartiere;
+import java.util.Collection;
+import java.util.Date;
 import javax.ejb.Local;
 
 /**
- *
+ * 
+ *     protected Collection<String> foto;
+        
+    protected double metratura;
+    * 
+    * 
  * @author gianp_000
  */
 @Local
@@ -17,4 +26,15 @@ public interface GestoreAnnuncioLocal {
     
     public boolean CreaAnnuncio(Locatore locatore);
     
+    public boolean inserisciInfoIndirizzo(String citta, String quartiere,String indirizzo,double[] latlng );
+    
+    public boolean inserisciInfoAppartamento(String descrizione, double metratura, Date dataInizioAffitto, int numeroStanze);
+    
+    /*
+    public boolean inserisciNuovaStanza(Collection<String> foto, double metratura, String tipo, double prezzo);
+    
+    public boolean eliminaStanza();
+    
+    public boolean modificaStanza();
+    */
 }
