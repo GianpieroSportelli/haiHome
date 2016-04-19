176a9b098acdd65d7774c87becf8f96cfd5efb61
SPORT <SPORT@192.168.1.87>
2016-04-20 00:54:32 +0200

    aggiunta ricerca di banche e bus station nei paraggi di un annuncio con relativa aggiunta come marker alla mappa

--- haiHome-ejb/src/java/ejb/GoogleMapsBean.java
+++ haiHome-ejb/src/java/ejb/GoogleMapsBean.java
@@ -201,4 +201,156 @@ public class GoogleMapsBean implements GoogleMapsBeanLocal {
         return null;
     }
 
+    @Override
+    public ArrayList<JSONObject> getBankNearBy(double lat, double lng, double rad) {
+        ArrayList<JSONObject> list = new ArrayList<>();
+        try {
+            String types = "types=bank";
+            //Possibili Type se si chiedono più tipi contemporaneamente separarli con | (es. types=grocery_or_supermarket|atm)
+            //accounting,airport,amusement_park,aquarium,art_gallery,!!atm!!,bakery
+            //bank,bar,beauty_salon,bicycle_store,book_store,bowling_alley,bus_station
+            //cafe,campground,car_dealer,car_rental,car_repair,car_wash,casino,cemetery
+            //church,city_hall,clothing_store,convenience_store,courthouse,dentist,department_store
+            //doctor,electrician,electronics_store,embassy,establishment,finance,fire_station,florist
+            //food,funeral_home,furniture_store,gas_station,general_contractor,grocery_or_supermarket
+            //gym,hair_care,hardware_store,health,hindu_temple,home_goods_store,hospital,insurance_agency
+            //jewelry_store,laundry,lawyer,library,liquor_store,local_government_office,locksmith,lodging
+            //meal_delivery,meal_takeaway,mosque.movie_rental,movie_theater.moving_company,museum,night_club
+            //painter,park,parking,pet_store,pharmacy,physiotherapist,place_of_worship,plumber,police
+            //post_office,real_estate_agency,restaurant,roofing_contractor,rv_park,school,shoe_store
+            //shopping_mall,spa,stadium.storage,!!store!!,subway_station,synagogue,taxi_stand,train_station
+            //travel_agency,university,veterinary_care,zoo
+            String location = "location=" + lat + "," + lng;
+            String radius = "radius=" + rad;
+            String parameters = location + "&" + radius + "&" + types + "&" + key;
+            String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + parameters;
+            JSONObject json = readJsonFromUrl(url);
+            
+            String status = json.getString("status");
+            if (status.equalsIgnoreCase("OK")) {
+                JSONArray results = json.getJSONArray("results");
+                //Elaborazione dei risultati ottenuti, problema: google restituisce 20 risultati alla volta
+                do {
+                    for (int i = 0; i < results.length(); i++) {
+                        //generazione JSON object di output, verrà append alla lista di output
+                        JSONObject res_i = results.getJSONObject(i);
+                        JSONObject get = new JSONObject();
+                        get.accumulate("location", res_i.getJSONObject("geometry").get("location"));
+                        get.accumulate("name", res_i.getString("name"));
+                        get.accumulate("vicinity", res_i.getString("vicinity"));
+                        list.add(get);
+                    }
+                    //viene posto results a null poichè sono stati eleborati tutti 
+                    results = null;
+                    //Se i risultati non sono terminati l'oggetto di risposta conterrà un campo 
+                    //next_page_token che serve per richedere gli altri 
+                    if (json.has("next_page_token")) {
+                        //google impedisce le richieste dirette di più risultati ma bisogna attendere
+                        //se non si attende ritorna un json object con status="INVALID REQUEST"
+                        //attesa ottenuta tramite Thred.sleep(x)
+                        try {
+                            Thread.sleep(2000);                 //1000 milliseconds is one second.
+                        } catch (InterruptedException ex) {
+                            Thread.currentThread().interrupt();
+                        }
+                        //costruzione url richiesta di altri risultati
+                        String pagetoken = "pagetoken=" + json.getString("next_page_token");
+                        String next_page = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + pagetoken + "&" + key;
+                        json = readJsonFromUrl(next_page);
+                        if (json.getString("status").equalsIgnoreCase("OK")) {
+                            //se lo status=OK si riempie results con i nuovi risultati
+                            results = json.getJSONArray("results");
+                            //altrimenti sarà settato a null dal istruzione prima del blocco di richiesta
+                            //nuova pagina risultati
+                        }
+                    }
+                } while (results != null);
+                /*for (JSONObject obj : list) {
+                 System.out.println(obj);
+                 }*/
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(GoogleMapsBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JSONException ex) {
+            Logger.getLogger(GoogleMapsBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+    
+    @Override
+    public ArrayList<JSONObject> getBusNearBy(double lat, double lng, double rad) {
+        ArrayList<JSONObject> list = new ArrayList<>();
+        try {
+            String types = "types=bus_station";
+            //Possibili Type se si chiedono più tipi contemporaneamente separarli con | (es. types=grocery_or_supermarket|atm)
+            //accounting,airport,amusement_park,aquarium,art_gallery,!!atm!!,bakery
+            //bank,bar,beauty_salon,bicycle_store,book_store,bowling_alley,bus_station
+            //cafe,campground,car_dealer,car_rental,car_repair,car_wash,casino,cemetery
+            //church,city_hall,clothing_store,convenience_store,courthouse,dentist,department_store
+            //doctor,electrician,electronics_store,embassy,establishment,finance,fire_station,florist
+            //food,funeral_home,furniture_store,gas_station,general_contractor,grocery_or_supermarket
+            //gym,hair_care,hardware_store,health,hindu_temple,home_goods_store,hospital,insurance_agency
+            //jewelry_store,laundry,lawyer,library,liquor_store,local_government_office,locksmith,lodging
+            //meal_delivery,meal_takeaway,mosque.movie_rental,movie_theater.moving_company,museum,night_club
+            //painter,park,parking,pet_store,pharmacy,physiotherapist,place_of_worship,plumber,police
+            //post_office,real_estate_agency,restaurant,roofing_contractor,rv_park,school,shoe_store
+            //shopping_mall,spa,stadium.storage,!!store!!,subway_station,synagogue,taxi_stand,train_station
+            //travel_agency,university,veterinary_care,zoo
+            String location = "location=" + lat + "," + lng;
+            String radius = "radius=" + rad;
+            String parameters = location + "&" + radius + "&" + types + "&" + key;
+            String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + parameters;
+            JSONObject json = readJsonFromUrl(url);
+            
+            String status = json.getString("status");
+            if (status.equalsIgnoreCase("OK")) {
+                JSONArray results = json.getJSONArray("results");
+                //Elaborazione dei risultati ottenuti, problema: google restituisce 20 risultati alla volta
+                do {
+                    for (int i = 0; i < results.length(); i++) {
+                        //generazione JSON object di output, verrà append alla lista di output
+                        JSONObject res_i = results.getJSONObject(i);
+                        JSONObject get = new JSONObject();
+                        get.accumulate("location", res_i.getJSONObject("geometry").get("location"));
+                        get.accumulate("name", res_i.getString("name"));
+                        get.accumulate("vicinity", res_i.getString("vicinity"));
+                        list.add(get);
+                    }
+                    //viene posto results a null poichè sono stati eleborati tutti 
+                    results = null;
+                    //Se i risultati non sono terminati l'oggetto di risposta conterrà un campo 
+                    //next_page_token che serve per richedere gli altri 
+                    if (json.has("next_page_token")) {
+                        //google impedisce le richieste dirette di più risultati ma bisogna attendere
+                        //se non si attende ritorna un json object con status="INVALID REQUEST"
+                        //attesa ottenuta tramite Thred.sleep(x)
+                        try {
+                            Thread.sleep(2000);                 //1000 milliseconds is one second.
+                        } catch (InterruptedException ex) {
+                            Thread.currentThread().interrupt();
+                        }
+                        //costruzione url richiesta di altri risultati
+                        String pagetoken = "pagetoken=" + json.getString("next_page_token");
+                        String next_page = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + pagetoken + "&" + key;
+                        json = readJsonFromUrl(next_page);
+                        if (json.getString("status").equalsIgnoreCase("OK")) {
+                            //se lo status=OK si riempie results con i nuovi risultati
+                            results = json.getJSONArray("results");
+                            //altrimenti sarà settato a null dal istruzione prima del blocco di richiesta
+                            //nuova pagina risultati
+                        }
+                    }
+                } while (results != null);
+                /*for (JSONObject obj : list) {
+                 System.out.println(obj);
+                 }*/
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(GoogleMapsBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JSONException ex) {
+            Logger.getLogger(GoogleMapsBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
 }
