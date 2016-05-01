bc37e3107f6728eeda15b290602bcc7071eba204
giacomocavallocg <giacomocavallo@mbp-di-giacomo.lan>
2016-05-01 02:20:20 +0200

    Aggiornato l'inserimento Annunci: 	Ora c'è il supporto GoogleAutocomplete che guida l'utente nell'inserimento 	corretto della via e del Cap. In maniera nascosta rispetto all'utente 	invio il CAP alla servlet per ricevere la lista dei quartieri possibili
    Bye Bye ;-)

--- haiHome-war/web/include/js/InserimentoAnnunci/googleAutocomplete.js
+++ haiHome-war/web/include/js/InserimentoAnnunci/googleAutocomplete.js
@@ -1,99 +1,91 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+      // This example displays an address form, using the autocomplete feature
+      // of the Google Places API to help users fill in the information.
 
-    
-    
-    var placeSearch, autocomplete;
-    var componentForm = {
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+      var placeSearch, autocomplete;
+      var componentForm = {
         street_number: 'short_name',
         route: 'long_name',
         locality: 'long_name',
         administrative_area_level_1: 'short_name',
         country: 'long_name',
         postal_code: 'short_name'
-    };
-    
-        var componentString = {
+      };
+      
+              var componentString = {
         street_number: 'short_name',
-        route: 'long_name'
+        route: 'long_name',
+        postal_code: 'short_name'
     };
-    
-    
-function initAutocomplete() {
-    
-    //Seleziono l'input su cui inserire l'autocomplite
-    //var input = ;
-    
-    //console.log(input.attr("id"));
-    
-    //inserisco le opzioni dell'autocomplite
-    var options = {
-        types: ['(address)'],
-        componentRestrictions: {country: 'it'}
-    };
-    
-    // Create the autocomplete object, restricting the search to geographical
-    // location types. 
-    autocomplete = new google.maps.places.Autocomplete(document.getElementById('inpIndirizzoProva'), options);
 
-    // When the user selects an address from the dropdown, populate the address
-    // fields in the form.
-    autocomplete.addListener('place_changed', fillInAddress);
-}
+      function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('inpIndirizzo')),
+            {
+                types: ['geocode'],
+                componentRestrictions: {country: 'it'}
+            });
 
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        autocomplete.addListener('place_changed', fillInAddress);
+      }
 
-      // [START region_fillform]
+   // [START region_fillform]
     function fillInAddress() {
         // Get the place details from the autocomplete object.
         var place = autocomplete.getPlace();
-
         
-        for (var component in componentForm) {
-            document.getElementById(component).value = '';
-            document.getElementById(component).disabled = false;
-        }
-
         // Get each component of the address from the place details
         // and fill the corresponding field on the form.
         for (var i = 0; i < place.address_components.length; i++) {
             var addressType = place.address_components[i].types[0];
-            if (componentForm[addressType]) {
-                var val = place.address_components[i][componentForm[addressType]];
-                document.getElementById(addressType).value = val;
-            }
             if (componentString[addressType]) {
                 var val = place.address_components[i][componentString[addressType]];
+                
                 componentString[addressType] = val;
                 
             }
         }
-        
+        var indirizzo = componentString['route'];
+        var civico = componentString['street_number'];
+        var CAP = componentString['postal_code'];
 
-            var indirizzo = componentString['route:'] + ", " + componentString['street_number'];
-        console.log("Indirizzo trovato " + indirizzo);
+        document.getElementById('inpIndirizzo').value = indirizzo;  
         
-        
-    }
-    
-        // [START region_geolocation]
-    // Bias the autocomplete object to the user's geographical location,
-    // as supplied by the browser's 'navigator.geolocation' object.
-    function geolocate() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var geolocation = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                var circle = new google.maps.Circle({
-                    center: geolocation,
-                    radius: position.coords.accuracy
-                });
-                autocomplete.setBounds(circle.getBounds());
-            });
+        if(civico != "short_name"){
+            document.getElementById('inpCivico').value = civico;  
         }
-    }
-    // [END region_geolocation]
+        
+        aggiornaListaQuartieri(CAP);
+        
+        componentString = {
+        street_number: 'short_name',
+        route: 'long_name',
+        postal_code: 'short_name'
+    };
+
+}
+
+      // Bias the autocomplete object to the user's geographical location,
+      // as supplied by the browser's 'navigator.geolocation' object.
+      function geolocate() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+              center: geolocation,
+              radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+          });
+        }
+      }
